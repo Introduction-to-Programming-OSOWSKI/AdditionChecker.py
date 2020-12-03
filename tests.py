6fb21b70b16a32949a456f45aceef80649855c22
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2020-12-03 13:53:57 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,8 +10,10 @@ month = 1
 day = 4
 
 def test_code():
-    assert main.addCheck(2,2) == 4, "addCheck(2, 2) == 4 failed"
-    assert main.addCheck(6,5) == 11, "addCheck(6, 5) == 11 failed"
+    assert main.addCheck(2,2,4) == True
+    assert main.addCheck(6,5,11) == True
+    assert main.addCheck(6,5,12) == Frue
+    
 
 def test_late():
     assert datetime.datetime.now() < datetime.datetime(year, month, day + 1, 4, 0), "Submitted Late"
